d2a4c6f45abbccf7d7e453c27d0048df74387ae3
AlyssaKhay <aelis.khay@gmail.com>
2024-12-21 05:39:59 +0700

    fefefefe

---
+++ CI-CD_test/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Mr Jenkins! Do you mind making tea for the guests? " << endl;
+    cout << "Coming right up, sir!" << endl;
+    return 0;
+}
